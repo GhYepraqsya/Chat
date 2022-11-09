96110aa229995c28b570534516d45c035e6fc50f
GhYepraqsya <yepraqsya.gharibjan.1999@gmail.com>
2022-11-09 18:27:11 +0400

    sign-up style changes

--- client/src/componets/SignUp/index.js
+++ client/src/componets/SignUp/index.js
@@ -5,10 +5,23 @@ import {
   RsgisterButton,
   RsgisterForm,
   RsgisterInput,
+  RsgisterInputPassword,
+  RsgisterInputSelect,
   RsgisterTitle
 } from './style'
 import { useSelector } from 'react-redux';
-import { Button, Input, Form } from 'antd';
+import {
+  AutoComplete,
+  Button,
+  Cascader,
+  Checkbox,
+  Col,
+  Form,
+  Input,
+  InputNumber,
+  Row,
+  Select,
+} from 'antd';
 import * as actions from '../../redux/action'
 import { connect } from 'react-redux';
 
@@ -19,6 +32,29 @@ const SignUp = ({ signUp }) => {
   //     email: "",
   //     password: ""
   // })
+
+  const formItemLayout = {
+    labelCol: {
+      xs: { span: 24 },
+      sm: { span: 8 },
+    },
+    wrapperCol: {
+      xs: { span: 24 },
+      sm: { span: 16 },
+    },
+  };
+  const tailFormItemLayout = {
+    wrapperCol: {
+      xs: {
+        span: 24,
+        offset: 0,
+      },
+      sm: {
+        span: 16,
+        offset: 8,
+      },
+    },
+  };
 
   const [user, setUser] = useReducer((state, action) => ({
     ...state,
@@ -43,47 +79,124 @@ const SignUp = ({ signUp }) => {
     signUp(user);
   }
 
+
+
   return (
     <>
       <RsgisterBody>
         <RsgisterTitle>Sign Up</RsgisterTitle>
         <Form
-          name="basic"
-          labelCol={{ span: 8 }}
-          wrapperCol={{ span: 16 }}
-          initialValues={{ remember: true }}
-          onFinish={() => {}}
-          onFinishFailed={() => {}}
-          autoComplete="off"
+          {...formItemLayout}
+          // form={form}
+          name="register"
+          // onFinish={onFinish}
+          // initialValues={{
+          //   residence: ['zhejiang', 'hangzhou', 'xihu'],
+          //   prefix: '86',
+          // }}
+          scrollToFirstError
         >
           <Form.Item
-            label="Username"
-            name="username"
-            rules={[{ required: true, message: 'Please input your username!' }]}
+            name="firstName"
+            // label="Firstname"
+            // tooltip="What do you want others to call you?"
+            rules={[{ required: true, message: 'Please input your firstname!', whitespace: true }]}
           >
-            <Input />
+            <RsgisterInput placeholder='Firstname' />
           </Form.Item>
 
           <Form.Item
-            label="Password"
-            name="password"
-            rules={[
-              { required: true, message: 'Please input your password!' },
-              { min: 8, message: 'You should type at least 8 characters' },
-              { max: 16, message: 'You should type at maximum 16 characters' },
-            ]
-            }
+            name="lastName"
+            // label="Lastname"
+            // tooltip="What do you want others to call you?"
+            rules={[{ required: true, message: 'Please input your lastname!', whitespace: true }]}
           >
-            <Input.Password />
+            <RsgisterInput placeholder='Lastname' />
           </Form.Item>
 
-          {/* <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 8, span: 16 }}>
-            <Checkbox>Remember me</Checkbox>
-          </Form.Item> */}
+          <Form.Item
+            name="email"
+            // label="E-mail"
+            rules={[
+              {
+                type: 'email',
+                message: 'The input is not valid E-mail!',
+              },
+              {
+                required: true,
+                message: 'Please input your E-mail!',
+              },
+            ]}
+          >
+            <RsgisterInput placeholder='E-mail' />
+          </Form.Item>
 
-          <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+          <Form.Item
+            name="password"
+            // label="Password"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your password!',
+              },
+            ]}
+            hasFeedback
+          >
+            <RsgisterInputPassword placeholder='Password' />
+          </Form.Item>
+
+          <Form.Item
+            name="confirm"
+            // label="Confirm Password"
+            dependencies={['password']}
+            hasFeedback
+            rules={[
+              {
+                required: true,
+                message: 'Please confirm your password!',
+              },
+              ({ getFieldValue }) => ({
+                validator(_, value) {
+                  if (!value || getFieldValue('password') === value) {
+                    return Promise.resolve();
+                  }
+                  return Promise.reject(new Error('The two passwords that you entered do not match!'));
+                },
+              }),
+            ]}
+          >
+            <RsgisterInputPassword placeholder='Confirm Password' />
+          </Form.Item>
+
+          <Form.Item
+            name="gender"
+            // label="Gender"
+            rules={[{ required: true, message: 'Please select gender!' }]}
+          >
+            <RsgisterInputSelect placeholder="select your gender">
+              <Select.Option value="male">Male</Select.Option>
+              <Select.Option value="female">Female</Select.Option>
+              <Select.Option value="other">Other</Select.Option>
+            </RsgisterInputSelect>
+          </Form.Item>
+          <Form.Item
+            name="agreement"
+            valuePropName="checked"
+            rules={[
+              {
+                validator: (_, value) =>
+                  value ? Promise.resolve() : Promise.reject(new Error('Should accept agreement')),
+              },
+            ]}
+            {...tailFormItemLayout}
+          >
+            <Checkbox>
+              I have read the <a href="">agreement</a>
+            </Checkbox>
+          </Form.Item>
+          <Form.Item {...tailFormItemLayout}>
             <Button type="primary" htmlType="submit">
-              Submit
+              Register
             </Button>
           </Form.Item>
         </Form>
